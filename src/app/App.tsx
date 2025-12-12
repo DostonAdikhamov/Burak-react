b13159863db67e0239388d9d62064d1b3489baad
Doston Adikhamov <adikhamov94@gmail.com>
2025-12-12 17:56:39 +0900

    fix: modify basket business logic via customized hooks

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { Route, Link, Switch, useLocation } from "react-router-dom";
 import  Home from "./screens/homePage/index";
-import  UserPage  from "./screens/userPage/index";
 import  ProductsPage  from "./screens/productsPage";
 import  OrdersPage  from "./screens/ordersPage";
+import  UserPage  from "./screens/userPage/index";
 import  HomeNavbar from "./components/headers/HeaderNavbar";
 import  OtherNavbar from "./components/headers/OtherNavbar";
 import  Footer  from "./components/footer";
@@ -12,41 +12,29 @@ import "../css/app.css";
 import "../css/navbar.css";
 import "../css/footer.css"
 import Test from "./screens/Test";
-import { CartItem } from "../lib/types/search";
-
+import useBasket from "./hooks/useBasket";
 
 function App() {
   const location = useLocation();
-
-  const cartJson: string | null = localStorage.getItem("cartData");
-  const currentCart = cartJson ? JSON.parse(cartJson) : [];
-  const [cartItems, setCartItems] = useState<CartItem[]>(currentCart);
-
-  /** HANDLERS **/
-
-  const onAdd = (input: CartItem) => {
-    const exist: any = cartItems.find(
-      (item: CartItem) => item._id === input._id
-    );
-    if(exist) {
-      const cartUpdate = cartItems.map((item: CartItem) => 
-        item._id === input._id 
-          ? {...exist, quantity: exist.quantity + 1 } 
-          : item
-      );
-      setCartItems(cartUpdate);
-      localStorage.setItem("cartData", JSON.stringify(cartUpdate));
-    } else {
-      const cartUpdate = [...cartItems, {...input}];
-      setCartItems(cartUpdate);
-      localStorage.setItem("cartData", JSON.stringify(cartUpdate));
-    }
-  }
-
+  const {cartItems, onAdd, onRemove, onDelete, onDeleteAll} = useBasket()
 
   return (
     <>
-      {location.pathname === "/" ? <HomeNavbar cartItems={cartItems} /> : <OtherNavbar cartItems={cartItems} />}
+      {location.pathname === "/" ? (
+      <HomeNavbar 
+        cartItems={cartItems} 
+        onAdd={onAdd} 
+        onRemove={onRemove} 
+        onDelete={onDelete} 
+        onDeleteAll={onDeleteAll}/>
+      ) : (
+      <OtherNavbar 
+        cartItems={cartItems} 
+        onAdd={onAdd} 
+        onRemove={onRemove} 
+        onDelete={onDelete} 
+        onDeleteAll={onDeleteAll}/>
+      )}
         <Switch>
           <Route path="/products">
             <ProductsPage onAdd={onAdd} />
